de63a8ca6e8b2ccbc49dc3e16c9e88a42ec18b90
Lnight <94696816+EvgenyPlaksin@users.noreply.github.com>
2023-01-26 17:22:20 +0100

    fix duplicate bug

--- app/src/main/java/com/lnight/material3clock/core/presentation/MainActivity.kt
+++ app/src/main/java/com/lnight/material3clock/core/presentation/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.lnight.material3clock.ui.theme.Material3ClockTheme
+import com.lnight.material3clock.core.presentation.ui.theme.Material3ClockTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
